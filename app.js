512752a714749321e964f92449d8cc3fed9e909f
tasnim <labiditasnim50@gmail.com>
2024-02-20 00:53:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello our fisrt microservices app');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server run on port${port}`);
+});
